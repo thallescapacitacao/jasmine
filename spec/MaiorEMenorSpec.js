aa958e1e1e1ec6521755c1a95c0495db141719c4
thallescapacitacao <38726485+thallescapacitacao@users.noreply.github.com>
2018-05-21 18:33:41 -0300

    https://cursos.alura.com.br/course/testes-automatizados-em-javascript-com-jasmine/task/5268

---
+++ spec/MaiorEMenorSpec.js
@@ -0,0 +1,42 @@
+describe("Maior e Menor", function() {
+
+    it("deve entender números em ordem não sequencial", () => {
+
+        var algoritmo = new MaiorEMenor();
+
+        algoritmo.encontra(5,15,7,9);
+
+        expect(algoritmo.pegaMaior()).toEqual(15);
+        expect(algoritmo.pegaMenor()).toEqual(5);
+    });
+
+    it("deve entender números em ordem decrescente", () => {
+
+        var algoritmo = new MaiorEMenor();
+
+        algoritmo.encontra(9,8,7,6);
+
+        expect(algoritmo.pegaMaior()).toEqual(9);
+        expect(algoritmo.pegaMenor()).toEqual(6);
+    });
+
+    it("deve suportar sucessivas chamadas ao método pegaMaior", () => {
+
+        var algoritmo = new MaiorEMenor();
+
+        algoritmo.encontra(9,8,7,6);
+
+        for (let i = 0; i < 3; i++)
+            expect(algoritmo.pegaMaior()).toEqual(9);
+    });
+
+    it("deve suportar chamadas aos métodos pegaMaior e pegaMenor com nenhum parâmetro passado", () => {
+
+        var algoritmo = new MaiorEMenor();
+
+        algoritmo.encontra();
+
+        expect(algoritmo.pegaMaior()).toEqual(undefined);
+        expect(algoritmo.pegaMenor()).toEqual(undefined);
+    });
+});
---
+++ src/MaiorEMenor.js
@@ -0,0 +1,27 @@
+class MaiorEMenor {
+
+    constructor() {
+
+        this._numerosEntrada = [];
+        this._numerosSaida = [];
+    }
+    
+    encontra(...numeros) {
+        
+        this._numerosEntrada = numeros;
+
+        this._numerosSaida = this._numerosEntrada.sort((a, b) => a - b);
+    }
+
+    pegaMaior() {
+
+        //return this._numerosSaida.pop();
+        //return [].concat(this._numerosSaida).pop();
+        //return this._numerosSaida.reverse()[0];
+    }
+
+    pegaMenor() {
+
+        return this._numerosSaida[0];
+    }
+}
